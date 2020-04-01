e4136f2d81740e26359ccbf2b1645a6b4a14cd5d
alilah <alalelfassi@gmail.com>
2020-03-31 21:53:46 -0400

    commented

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-//import Image from "../components/image"
-//import SEO from "../components/seo"
+import Image from "../components/image"
+import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    /*<SEO title="Home" />*/
+    <SEO title="Home" />
     <h1>Hi people and Al</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now let's build something great.</p>
     <p> Here is a link <a href="https://wonderful-morse-7f443e.netlify.com"> to go to
     </a>
     </p>
-    <!--
-    /*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-    </div>*/-->
+    </div>*/}
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )

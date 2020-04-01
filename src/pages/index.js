68ef5f369743366043fa34ee314dce39c4d9f82b
alilah <alalelfassi@gmail.com>
2020-03-31 21:40:34 -0400

    remove image

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,16 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    //<SEO title="Home" />
     <h1>Hi people and Al</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now let's build something great.</p>
-    <p> here is a link <a href="https://wonderful-morse-7f443e.netlify.com"> to go to
+    <p> Here is a link <a href="https://wonderful-morse-7f443e.netlify.com"> to go to
     </a>
-    </p>
+    /*</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-    </div>
+    </div>*/
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
